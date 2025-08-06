1301807f65533d7eec6c15bcd7aaaf21b42b1191
mugesz <mugesh@quartrdesign.com>
2025-08-06 13:18:08 +0530

    changes

--- routes/crimenews.js
+++ routes/crimenews.js
@@ -5,78 +5,95 @@ const CrimeNews = require("../model/crime");
 require("dotenv").config();
 const uri = process.env.DB;
 
-mongoose.connect(uri);
+// Connect to MongoDB with error handling
+mongoose
+  .connect(uri)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
+// Create news
 router.post("/create-news", async (req, res) => {
   try {
-    const news = new CrimeNews({
-      title: req.body.title,
-      
-      description: req.body.description,
-    });
-
+    const { title, description } = req.body;
+    if (!title || !description) {
+      return res
+        .status(400)
+        .json({ message: "Title and description are required" });
+    }
+    const news = new CrimeNews({ title, description });
     const saveNews = await news.save();
-    await res.status(200).send(saveNews);
+    res.status(200).send(saveNews);
   } catch (error) {
     console.log(error);
-    res.status(500).json({
-      message: "something went wrong",
-    });
+    res.status(500).json({ message: "something went wrong" });
   }
 });
 
+// Get all news
 router.get("/getAllNews", async (req, res) => {
   try {
     const news = await CrimeNews.find();
     res.status(200).send(news);
   } catch (error) {
     console.log(error);
-    res.status(500).json({
-      message: "something went wrong",
-    });
+    res.status(500).json({ message: "something went wrong" });
   }
 });
 
+// Get one news by ID
 router.get("/getOneNews/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const news = await CrimeNews.findById(id);
+    if (!news) {
+      return res.status(404).json({ message: "News not found" });
+    }
     res.status(200).send(news);
   } catch (error) {
     console.log(error);
-    res.status(500).json({
-      message: "something went wrong",
-    });
+    res.status(500).json({ message: "something went wrong" });
   }
 });
 
-router.put("/editNews", async (req, res) => {
+// Edit news by ID
+router.put("/editNews/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const edit = {
-      title: req.body.title,
-      description: req.body.description,
-    };
-    const news = await CrimeNews.findByIdAndUpdate(edit, id);
+    const { title, description } = req.body;
+    if (!title || !description) {
+      return res
+        .status(400)
+        .json({ message: "Title and description are required" });
+    }
+    const news = await CrimeNews.findByIdAndUpdate(
+      id,
+      { title, description },
+      { new: true }
+    );
+    if (!news) {
+      return res.status(404).json({ message: "News not found" });
+    }
     res.status(200).send(news);
   } catch (error) {
     console.log(error);
-    res.status(500).json({
-      message: "something went wrong",
-    });
+    res.status(500).json({ message: "something went wrong" });
   }
 });
 
+// Delete news by ID
 router.delete("/deleteNews/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const news = await CrimeNews.findByIdAndDelete(id);
+    if (!news) {
+      return res.status(404).json({ message: "News not found" });
+    }
     res.status(200).send(news);
   } catch (error) {
     console.log(error);
-    res.status(500).json({
-      message: "something went wrong",
-    });
+    res.status(500).json({ message: "something went wrong" });
   }
 });
+
+module.exports = router;
 
